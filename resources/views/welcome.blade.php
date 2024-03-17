aec7fdf3f117bd5e9baf56f6eb773de2f2420157
wynnepriscillia <wynnepriscillia.3110@gmail.com>
2024-03-17 22:10:27 +0700

    trial ganti judul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>SecondLife - Barter Platform</title>
+  <title>SecondLife - WINI HHAHAHA</title>
   <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" href="/argon/assets/css/argon-dashboard.css">
   <!--
