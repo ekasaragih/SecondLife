f0783c0a1fa023ee184ef71d26603d8ce0518d9a
ekasaragih <saragiheka27@gmail.com>
2024-04-01 16:53:21 +0700

    PS-20: Add button message on topnav

--- resources/views/layouts/navbar/topnav.blade.php
+++ resources/views/layouts/navbar/topnav.blade.php
@@ -27,20 +27,26 @@
     </div>
 
     <div class="flex items-center gap-4">
-        <button class="relative text-2xl text-gray-700">
-            <i class="fa fa-user" aria-hidden="true"></i>
+        <button class="relative text-3xl text-gray-700" title="Messages">
+            <i class="fa fa-comments-o" aria-hidden="true"></i>
+            <span
+                class="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-red-500 text-white text-xs font-semibold px-2 py-1 rounded-full">0</span>
         </button>
 
-        <button class="relative text-2xl text-gray-700">
+        <button class="relative text-3xl text-gray-700" title="Wishlist">
             <i class="fa fa-heart" aria-hidden="true"></i>
             <span
                 class="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-red-500 text-white text-xs font-semibold px-2 py-1 rounded-full">0</span>
         </button>
 
-        <button class="relative text-2xl text-gray-700">
+        <button class="relative text-3xl text-gray-700" title="Cart">
             <i class="fa fa-shopping-bag" aria-hidden="true"></i>
             <span
                 class="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-red-500 text-white text-xs font-semibold px-2 py-1 rounded-full">0</span>
+        </button>
+
+        <button class="relative text-3xl text-gray-700" title="My Profile">
+            <i class="fa fa-user-circle" aria-hidden="true"></i>
         </button>
     </div>
 </div>
