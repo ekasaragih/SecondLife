77cac7938aeddefa3c4a128a168e73a5f967cefc
ekasaragih <saragiheka27@gmail.com>
2024-04-11 13:31:53 +0700

    PS-44: Add change location form

--- resources/views/pages/user-profile.blade.php
+++ resources/views/pages/user-profile.blade.php
@@ -34,6 +34,7 @@
                         </span>
                     </div>
                     <div class="text-primary">@charajiwin</div>
+                    <a href="#" class="text-gray-500 underline text-sm mt-2 italic">Change password</a>
                     <div class="py-2 mt-5">
                         <div class="inline-flex text-gray-700 dark:text-gray-300 items-center">
                             <svg class="h-5 w-5 text-gray-400 dark:text-gray-600 mr-1" fill="currentColor"
@@ -61,61 +62,53 @@
                             <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
                                 <div class="sm:col-span-2">
                                     <label for="name"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full
                                         Name</label>
                                     <input type="text" name="name" id="name"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                        value="Apple iMac 27&ldquo;" placeholder="Type product name" required="">
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        value="" placeholder="Type full name" required="">
                                 </div>
                                 <div class="w-full">
-                                    <label for="brand"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Brand</label>
-                                    <input type="text" name="brand" id="brand"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                        value="Apple" placeholder="Product brand" required="">
+                                    <label for="username"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                                    <input type="text" name="username" id="username"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        value="" placeholder="Type username" required="">
                                 </div>
                                 <div class="w-full">
-                                    <label for="price"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
-                                    <input type="number" name="price" id="price"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                        value="2999" placeholder="$299" required="">
+                                    <label for="age"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Age</label>
+                                    <input type="number" name="age" id="age"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        value="" placeholder="21" required="">
+                                </div>
+                                <div class="w-full">
+                                    <label for="email"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                                    <input type="email" name="email" id="email"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        value="" placeholder="admin@user.com" required="">
                                 </div>
                                 <div>
-                                    <label for="category"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
-                                    <select id="category"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                        <option selected="">Electronics</option>
-                                        <option value="TV">TV/Monitors</option>
-                                        <option value="PC">PC</option>
-                                        <option value="GA">Gaming/Console</option>
-                                        <option value="PH">Phones</option>
+                                    <label for="gender"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gender</label>
+                                    <select id="gender"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
+                                        <option selected value="None">None</option>
+                                        <option value="Female">Female</option>
+                                        <option value="Male">Male</option>
+                                        <option value="Rather not to say">Rather not to say</option>
                                     </select>
                                 </div>
-                                <div>
-                                    <label for="item-weight"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Item
-                                        Weight (kg)</label>
-                                    <input type="number" name="item-weight" id="item-weight"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                        value="15" placeholder="Ex. 12" required="">
-                                </div>
-                                <div class="sm:col-span-2">
-                                    <label for="description"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                                    <textarea id="description" rows="8"
-                                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                        placeholder="Write a product description here...">Standard glass, 3.8GHz 8-core 10th-generation Intel Core i7 processor, Turbo Boost up to 5.0GHz, 16GB 2666MHz DDR4 memory, Radeon Pro 5500 XT with 8GB of GDDR6 memory, 256GB SSD storage, Gigabit Ethernet, Magic Mouse 2, Magic Keyboard - US</textarea>
-                                </div>
+
                             </div>
                             <div class="flex items-center space-x-4">
                                 <button type="submit"
                                     class="text-black bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                                    Update product
+                                    Update profile
                                 </button>
                                 <button type="button"
-                                    class="text-yellow-400 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                    class="text-yellow-400 inline-flex items-center hover:text-white border border-yellow-400 hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                                     <svg class="w-5 h-5 mr-1 -ml-1" fill="currentColor" viewBox="0 0 20 20"
                                         xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd"
@@ -131,17 +124,99 @@
             </div>
 
             <div class="hidden" id="changeAddressCollapseMenu">
-                <p>
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-                    labore wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                    excepteur butcher vice lomo. Leggings occaecat craft beer
-                    farm-to-table, raw denim aesthetic synth nesciunt you probably haven't
-                    heard of them accusamus labore sustainable VHS. 3 wolf moon officia
-                    aute, non cupidatat skateboard dolor brunch. Food truck quinoa
-                    nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a
-                    bird on it squid single-origin coffee nulla assumenda shoreditch et.
-                </p>
+                <hr class="mt-8 border-gray-300 sm:mx-auto dark:border-gray-700" />
+                <section>
+                    <div class="max-w-2xl px-4 py-8 mx-auto">
+                        <h2 class="mb-4 text-2xl font-bold text-[#F12E52]">Change address</h2>
+                        <form action="#">
+                            <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
+                                <div class="sm:col-span-2">
+                                    <label for="name"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full
+                                        Name</label>
+                                    <input type="text" name="name" id="name"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        value="" placeholder="Type full name" required="">
+                                </div>
+                                <div class="w-full">
+                                    <label for="phone"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone
+                                        Number</label>
+                                    <input type="number" name="phone" id="phone"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        value="" placeholder="081234567890" required="">
+                                </div>
+                                <div class="w-full">
+                                    <label for="username"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Province,
+                                        City, District, Postal Code</label>
+                                    <input type="text" name="username" id="username"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        value="" placeholder="Type username" required="">
+                                </div>
+                                <div class="w-full">
+                                    <label for="username"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Street
+                                        Name, Building, House No.</label>
+                                    <input type="text" name="username" id="username"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        value="" placeholder="Type username" required="">
+                                </div>
+                                <div class="w-full">
+                                    <label for="username"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Other
+                                        Details (e.g. Block/Unit No., Landmarks)
+                                    </label>
+                                    <input type="text" name="username" id="username"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        value="" placeholder="Type username" required="">
+                                </div>
+                                <div class="w-full">
+                                    <label for="username"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Label As:
+                                    </label>
+                                    <ul class="grid w-full gap-6 md:grid-cols-2">
+                                        <li>
+                                            <input type="radio" id="home" name="label" value="Home" class="hidden peer"
+                                                required />
+                                            <label for="home"
+                                                class="inline-flex items-center justify-between w-full p-2 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100">
+                                                <div class="block">
+                                                    <div class="w-full text-md font-semibold">Home</div>
+                                                </div>
+                                            </label>
+                                        </li>
+                                        <li>
+                                            <input type="radio" id="work" name="label" value="Work" class="hidden peer">
+                                            <label for="work"
+                                                class="inline-flex items-center justify-between w-full p-2 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100">
+                                                <div class="block">
+                                                    <div class="w-full text-md font-semibold">Work</div>
+                                                </div>
+                                            </label>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="flex items-center space-x-4">
+                                <button type="submit"
+                                    class="text-black bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                    Change address
+                                </button>
+                                <button type="button"
+                                    class="text-yellow-400 inline-flex items-center hover:text-white border border-yellow-400 hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                    <svg class="w-5 h-5 mr-1 -ml-1" fill="currentColor" viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                    Reset
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </section>
             </div>
 
         </div>
