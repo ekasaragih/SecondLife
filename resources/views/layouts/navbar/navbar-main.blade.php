aa1f0aa7c3865a5cc3543f2fd5815fcb085a1d17
ekasaragih <saragiheka27@gmail.com>
2024-04-11 14:13:34 +0700

    PS-44: Update navbar on hover color

--- resources/views/layouts/navbar/navbar-main.blade.php
+++ resources/views/layouts/navbar/navbar-main.blade.php
@@ -2,17 +2,17 @@
     <ul class="flex justify-center gap-24">
         <li class="relative text-xl">
             <a href="{{ url('/') }}"
-                class="font-semibold uppercase transition duration-300 hover:text-primary-content {{ request()->is('/') ? 'text-primary underline transition duration-300' : 'focus:text-secondary' }}">Explore</a>
+                class="font-semibold uppercase transition duration-300 hover:text-primary hover:opacity-75 {{ request()->is('/') ? 'text-primary underline transition duration-300' : 'focus:text-secondary' }}">Explore</a>
         </li>
 
         <li class="relative text-xl">
             <a href="{{ route('categories') }}"
-                class="font-semibold uppercase transition duration-300 hover:text-primary-content {{ request()->routeIs('categories') ? 'text-primary underline transition duration-300' : 'focus:text-secondary' }}">Categories</a>
+                class="font-semibold uppercase transition duration-300 hover:text-primary hover:opacity-75 {{ request()->routeIs('categories') ? 'text-primary underline transition duration-300' : 'focus:text-secondary' }}">Categories</a>
         </li>
 
         <li class="relative text-xl">
             <a href="{{ route('communities') }}"
-                class="font-semibold uppercase transition duration-300 hover:text-primary-content {{ request()->routeIs('communities') ? 'text-primary underline transition duration-300' : 'focus:text-secondary' }}">Communities</a>
+                class="font-semibold uppercase transition duration-300 hover:text-primary hover:opacity-75 {{ request()->routeIs('communities') ? 'text-primary underline transition duration-300' : 'focus:text-secondary' }}">Communities</a>
         </li>
     </ul>
 </nav>
