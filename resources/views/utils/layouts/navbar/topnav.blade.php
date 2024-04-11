28c394c46c445e6782361986e5847dbce2317cde
ekasaragih <saragiheka27@gmail.com>
2024-04-11 14:54:09 +0700

    PS-44: Refactor files

--- resources/views/utils/layouts/navbar/topnav.blade.php
+++ resources/views/utils/layouts/navbar/topnav.blade.php
@@ -1,4 +1,7 @@
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="tailwind.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -51,5 +54,5 @@
         </div>
     </div>
 
-    @include('layouts.navbar.navbar-main')
+    @include('utils.layouts.navbar.navbar-main')
 </div>
