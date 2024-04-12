17e1eeceda87a71f3812b0ac584c4ff8e5a466ff
ekasaragih <saragiheka27@gmail.com>
2024-04-12 15:27:07 +0700

    PS-82: Add logout function

--- app/Http/Controllers/AuthorizationController.php
+++ app/Http/Controllers/AuthorizationController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Constants\Roles;
+use App\Models\User;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthorizationController extends Controller
 {
@@ -15,4 +20,14 @@ class AuthorizationController extends Controller
     {
         return view("authorization.register");
     }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login');
+    }
+
+
 }
