7c3a6f57cbe7d4aff9a3bff83fa03de433500763
wynnepriscillia <wynnepriscillia.3110@gmail.com>
2024-04-13 01:03:47 +0700

    PS-38: community

--- resources/views/pages/communities.blade.php
+++ resources/views/pages/communities.blade.php
@@ -1,12 +1,12 @@
 @include('utils.layouts.navbar.topnav')
 <div class="flex justify-center h-screen pt-52">
     <div class="container w-4/5">
-
         <div class="text-3xl text-[#F12E52] mb-2"><b>Share Your Thoughts</b></div>
         <div class="w-full mx-auto rounded">
             <form>
-                <div class="mb-4">
-                    <input type="textarea" id="post" name="post" class="w-full h-40 px-3 py-2 border-2 rounded-md ">
+                <div class="mb-4 border-2 rounded-md">
+                    <input type="text" id="post" name="post" class="w-full px-3 py-2 border-b border-gray-300 font-semibold" placeholder="Title">
+                    <input type="textarea" id="post" name="post" class="w-full h-40 px-3 py-2 pt-0" placeholder="Share your thoughts">
                 </div>
                 <button class="bg-[#F12E52] hover:bg-white text-white hover:text-[#F12E52] font-bold py-2 px-4 rounded shadow">
                     Post
@@ -14,26 +14,36 @@
             </form>
         </div>
         <div class="text-3xl text-[#F12E52] mt-5 mb-2"><b>See What's ☕ in the town</b></div>
-        <div class="flex flex-wrap mt-5">
-            <divn class="w-1/3">
-                <div class="rounded-lg shadow-md p-3 border-2 m-1">
-                    <h2 class="text-lg font-semibold mb-2">Help!</h2>
-                    <p class="text-gray-600">Do you think it's worth to barter my corkcickle with a book called "Before the coffee gets cold", I really want to have this book and I can not find it in any bookstore</p>
-                    <h2 class="underline">See More</h2>
-                    <div class="flex flex-row items-center">
-                        <span class="text-2xl text-[#F12E52] mr-1">&hearts;</span>
-                        <h3>20</h3>
-                    </div>
-                    <div class="flex items-center border-t border-gray-400 p-2">
-                        <img class="w-10 h-10 rounded-full mr-4" src="https://cdn.antaranews.com/cache/1200x800/2023/03/13/4E7F573F-B89D-40E4-AE6F-56C06AD96496.jpeg" alt="Profile Picture">
-                        <h2 class="text-lg font-semibold">Jeno Lee</h2>
-                        <h2 class="text-base text-gray-500">March 24, 2024</h2>
-                      </div>
-                </div>     
-            </divn>
+        <div id="post-card" class="mt-5">
+            <div id="post" class="rounded-lg shadow-md p-3 border-2 m-1">
+                <div class="flex items-center p-3">
+                    <img class="w-8 h-8 rounded-full mr-4" src="https://cdn.antaranews.com/cache/1200x800/2023/03/13/4E7F573F-B89D-40E4-AE6F-56C06AD96496.jpeg" alt="Profile Picture">
+                    <h2 class="text-lg font-semibold mr-3">Jeno Lee</h2>
+                    <h2 class="text-base text-gray-500 italic">March 24, 2024</h2>
+                </div>
+                <h2 class="text-lg font-semibold mb-2 pl-3">Help!</h2>
+                <p class="text-gray-800 pl-3">Do you think it's worth to barter my corkcickle with a book called "Before the coffee gets cold", I really want to have this book and I can not find it in any bookstore</p>
+                <div class="flex flex-row items-center pl-3">
+                    <span class="text-2xl text-[#F12E52] mr-1">&hearts;</span>
+                    <h3>20</h3>
+                </div>
+            </div>  
+            <div class="w-11/12 float-right flex items-center">
+                <b class="text-lg text-[#F12E52] underline">Replies</b>
+            </div>  
+            <div id="replies" class="w-11/12 rounded-lg shadow-md p-5 border-2 m-1 float-right flex items-center">
+                <img class="w-8 h-8 rounded-full mr-4" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUAl5RnJ7_Y5MWYXPkxtFAM5eahD7k9RjDDQ&s" alt="Profile Picture">
+                <h2 class="text-lg font-semibold mr-3">Mark Lee</h2>
+                <h2 class="text-base text-gray-500 italic">March 24, 2024</h2>
+                <p class="text-gray-800 pl-3">YASHH! You're totally missing a lot</p>
+            </div>
+            <div id="replies" class="w-11/12 rounded-lg shadow-md p-5 border-2 m-1 float-right flex items-center">
+                <img class="w-8 h-8 rounded-full mr-4" src="https://i.pinimg.com/736x/cc/c3/e6/ccc3e6081e3b11da3eb6f16d625b45c9.jpg" alt="Profile Picture">
+                <h2 class="text-lg font-semibold mr-3">Jisung Park</h2>
+                <h2 class="text-base text-gray-500 italic">March 24, 2024</h2>
+                <p class="text-gray-800 pl-3">I have that, wanna exchange?</p>
+            </div>
         </div>
-        
-        
     </div>
 </div>
 
