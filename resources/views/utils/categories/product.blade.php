f3b344bf82067935e908c9c45d04dd971aa30460
shikachika <eldi653@gmail.com>
2024-04-12 15:30:17 +0700

    PS-38-Categories Page- Rara

---
+++ resources/views/utils/categories/product.blade.php
@@ -0,0 +1,43 @@
+<div class="flex gap-2 px-2">
+<a href="electronic">
+    <button 
+        class="py-2.5 px-5 flex-1 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">
+        Electronic
+    </button>
+</a>
+
+<br><br>
+<a href="fashion">
+    <button
+        class="py-2.5 px-5 flex-1 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">
+        Fashion
+    </button>
+</a>
+
+<br><br>
+<a href="destination.html">
+    <button
+        class="py-2.5 px-5 flex-1 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">
+        My wishlist
+    </button>
+</a><br>
+
+
+<a href="destination.html">
+    <button
+        class="py-2.5 px-5 flex-1 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">
+        My wishlist
+    </button>
+</a>
+
+<a href="destination.html">
+    <button
+        class="py-2.5 px-5 flex-1 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">
+        My wishlist
+    </button>
+</a>
+
+
+
+</div>
+
