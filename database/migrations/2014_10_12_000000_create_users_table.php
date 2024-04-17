f6404662d44313f677a1f7721bb5e9979689914d
ekasaragih <saragiheka27@gmail.com>
2024-04-17 16:53:30 +0700

    PS-48: Update session table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -30,7 +30,7 @@ return new class extends Migration
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('us_ID')->nullable()->index();
+            $table->foreignId('us_ID');
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
