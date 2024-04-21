6b3d0ddd4b7b99ebad9d348051af3d5518502c07
ekasaragih <saragiheka27@gmail.com>
2024-04-21 11:43:45 +0700

    PS-111: Update styling in the about us page

--- resources/views/utils/menu/aboutUs.blade.php
+++ resources/views/utils/menu/aboutUs.blade.php
@@ -2,39 +2,95 @@
     <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="tailwind.css">
     <link rel="shortcut icon" href="/asset/img/mini-logo.png" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
     @yield('head')
 </head>
 
-<div id="about" class="relative bg-white overflow-hidden mt-16">
-    <div class="max-w-7xl mx-auto">
-        <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-            <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+<div id="about" class="relative bg-white overflow-hidden font-rubik">
+    <div class="max-w-7xl px-10">
+        <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full">
+            {{-- <svg
+                class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
                 fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
                 <polygon points="50,0 100,0 50,100 0,100"></polygon>
-            </svg>
+            </svg> --}}
 
             <div class="pt-1"></div>
 
-            <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div class="mt-5 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8">
                 <div class="sm:text-center lg:text-left">
                     <h2 class="my-6 text-2xl tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl">
-                        About me
+                        About us —
                     </h2>
 
-                    <p>
-                        Donec porttitor, enim ut dapibus lobortis, lectus sem tincidunt dui, eget ornare lectus ex non
-                        libero. Nam rhoncus diam ultrices porttitor laoreet. Ut mollis fermentum ex, vel viverra lorem
-                        volutpat sodales. In ornare porttitor odio sit amet laoreet. Sed laoreet, nulla a posuere
-                        ultrices, purus nulla tristique turpis, hendrerit rutrum augue quam ut est. Fusce malesuada
-                        posuere libero, vitae dapibus eros facilisis euismod. Sed sed lobortis justo, ut tincidunt
-                        velit. Mauris in maximus eros.
-                    </p>
+                    <h4 class="text-lg my-2 text-primary font-rubik font-bold">"Digital Barter System: Facilitating
+                        Sustainable Consumption through Web-Based Platform for Goods Exchange"</h4>
+                    <div class="space-y-6">
+                        <p>
+                            In today's world, many households face the issue of accumulating unused items due to
+                            impulsive
+                            buying and changing
+                            trends. This not only burdens homes but also contributes to environmental problems like
+                            increased waste in landfills. To
+                            tackle this, we need innovative solutions. Our project aims to address the challenges of
+                            inefficient use of goods,
+                            communication barriers in transactions, and difficulty finding exchange opportunities. The
+                            goal
+                            is clear: reduce
+                            resource wastage, promote sustainable consumer practices, improve local exchanges, and
+                            enhance
+                            precision in barter
+                            transactions.
+                        </p>
+                        <p>
+                            To overcome these challenges, we propose a digital barter platform with features like
+                            Decentralized exchange goods
+                            offering flexibility in defining specific terms, conditions, and rules for each transaction,
+                            Real-time intuitive
+                            seamless messaging systems, Real time geospatial intelligence services with redefine
+                            precision,
+                            accuracy, and efficiency
+                            for dynamic location-based, Auto price prediction feature based on the product age and
+                            materials, Auto recommendations
+                            feature based on user behavior and product offer’s category, Intuitive discerning
+                            swipe-based
+                            mechanism for matching
+                            products among Barterers, Smart assistant for auto prediction on product trends, and
+                            Community
+                            enrichment platform
+                            designed to foster meaningful feedback, insightful interactions, and the cultivation of a
+                            refined collective intellect.
+                            Overall, our approach aims to create a digital ecosystem that addresses societal concerns,
+                            improves consumer
+                            experiences, and fosters a more sustainable and engaged community.
+                        </p>
+                        <p>
+                            SecondLife is the platform we made for those who needed the changes for a sustainable life.
+                        </p>
+                        <p>
+                            Welcome to <a href="/"
+                                class="text-primary-content hover:text-opacity-75 hover:underline">SecondLife.</a>
+                        </p>
+                        <p>
+                            This project made by: Eka Parima Saragih, Ratu Esther Eldi, Regina Rosa Maria, Wynne
+                            Priscillia
+                            Johan. Student of
+                            Information System batch 2021.
+                        </p>
+                        <p class="text-sm opacity-40 text-secondary">
+                            © Copyright Rachajiwin-2024. All Rights Reserved.
+                            SecondLife
+                        </p>
+                    </div>
                 </div>
-            </main>
+            </div>
         </div>
     </div>
     <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <img class="h-56 w-full object-cover object-top sm:h-72 md:h-96 lg:w-full lg:h-full"
-            src="https://cdn.pixabay.com/photo/2016/03/23/04/01/woman-1274056_960_720.jpg" alt="">
+        <a href="/" title="Click to go back to home" class="hover:opacity-80"><img
+                class="w-full ml-[-50px] hover:scale-90 translate-x-4 duration-300 hover:skew-y-3 object-top"
+                src="/asset/img/main-logo.png" alt="Logo SecondLife"></a>
     </div>
 </div>
