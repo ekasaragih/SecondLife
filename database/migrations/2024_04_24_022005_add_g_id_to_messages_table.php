92112e24600d181831815a13305f7f092eb35e16
ekasaragih <saragiheka27@gmail.com>
2024-04-24 09:21:44 +0700

    PS-66: Add new migration for add column g_ID in message

---
+++ database/migrations/2024_04_24_022005_add_g_id_to_messages_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->unsignedBigInteger('g_ID')->nullable();
+            $table->foreign('g_ID')->references('g_ID')->on('goods')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+         Schema::table('messages', function (Blueprint $table) {
+            $table->dropForeign(['g_ID']);
+            $table->dropColumn('g_ID');
+        });
+    }
+};
