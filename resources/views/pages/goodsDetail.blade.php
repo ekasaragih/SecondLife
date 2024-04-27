90d501550160e0137fd3b6370cd64c272a33dc15
ekasaragih <saragiheka27@gmail.com>
2024-04-27 18:57:42 +0700

    PS-155: Add function to disabled button barter if it's user's own goods

--- resources/views/pages/goodsDetail.blade.php
+++ resources/views/pages/goodsDetail.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @auth
-        <meta name="api-token" content="{{ Auth::user()->api_token }}">
+    <meta name="api-token" content="{{ Auth::user()->api_token }}">
     @endauth
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -20,7 +20,8 @@
                     <div id="productImages" class="lg:w-1/2 w-full flex justify-center items-center">
                         <div class="flex items-center">
                             @foreach ($product->images as $image)
-                                <img class="product-image hidden h-64 object-cover object-center" src="{{ asset('goods_img/' . $image->img_url) }}">
+                            <img class="product-image hidden h-64 object-cover object-center"
+                                src="{{ asset('goods_img/' . $image->img_url) }}">
                             @endforeach
                         </div>
                     </div>
@@ -40,33 +41,51 @@
                             </div>
                         </div>
                         <div class="flex flex-col justify-center items-center">
-    <div class="text-center mb-4">
-        <span class="title-font font-medium text-xl text-gray-900">
-            Prediction price:
-        </span>
-        <span class="text-base">
-            {{-- Format the prediction price in rupiah --}}
-            Rp{{ number_format($product->g_price_prediction, 0, ',', '.') }}
-        </span>
-    </div>
-    <div class="flex">
-        <button class="text-white bg-red-500 border-0 py-2 px-2 text-sm focus:outline-none hover:bg-red-600 rounded transition duration-300"
-                data-modal-target="modalTermsAndCondition" data-modal-toggle="modalTermsAndCondition">
-            Click to Barter
-        </button>
+                            <div class="text-center mb-4">
+                                <span class="title-font font-medium text-xl text-gray-900">
+                                    Prediction price:
+                                </span>
+                                <span class="text-base">
+                                    {{-- Format the prediction price in rupiah --}}
+                                    Rp{{ number_format($product->g_price_prediction, 0, ',', '.') }}
+                                </span>
+                            </div>
+                            <div class="flex">
+                                <button
+                                    class="text-white bg-red-500 border-0 py-2 px-2 text-sm focus:outline-none disabled:bg-slate-400 hover:bg-red-600 rounded transition duration-300"
+                                    data-modal-target="modalTermsAndCondition" @if($product->us_ID === auth()->id())
+                                    data-popover-target="popoverDisabled"
+                                    @endif
+                                    data-modal-toggle="modalTermsAndCondition" {{ $product->us_ID === auth()->id() ?
+                                    'disabled' : '' }}>
+                                    Click to Barter
+                                </button>
+                                <div data-popover id="popoverDisabled" role="tooltip"
+                                    class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+                                    <div
+                                        class="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+                                        <h3 class="font-semibold text-gray-900 dark:text-white">Why i can't click this?
+                                        </h3>
+                                    </div>
+                                    <div class="px-3 py-2">
+                                        <p>This is your own goods. You can't barter with your own goods.</p>
+                                    </div>
+                                    <div data-popper-arrow></div>
+                                </div>
 
-        <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4 add-to-wishlist"
-                title="Add to wishlist" id="btn_add_wishlist" data-product-id="{{ $product->g_ID }}"
-                data-user-id="{{ $authenticatedUser->us_ID }}">
-            <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                class="w-5 h-5 love-icon" viewBox="0 0 24 24">
-                <path
-                    d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z">
-                </path>
-            </svg>
-        </button>
-    </div>
-</div>
+                                <button
+                                    class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4 add-to-wishlist duration-300 hover:text-red-500 hover:bg-red-50 hover:border hover:border-red-500"
+                                    title="Add to wishlist" id="btn_add_wishlist" data-product-id="{{ $product->g_ID }}"
+                                    data-user-id="{{ $authenticatedUser->us_ID }}">
+                                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" class="w-5 h-5 love-icon" viewBox="0 0 24 24">
+                                        <path
+                                            d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z">
+                                        </path>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -77,29 +96,29 @@
             </div>
         </section>
 
-    @include('utils.layouts.footer.footer')
+        @include('utils.layouts.footer.footer')
 
-</div>
+    </div>
 
-@auth
+    @auth
     {{-- T&C Modal --}}
     @include('utils.categories.modalTermsAndCondition')
-@endauth
+    @endauth
 
-{{--
-|--------------------------------------------------------------------------
-| SCRIPTS
-|--------------------------------------------------------------------------
---}}
-<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
-<script src="/js/moment.js"></script>
-<script>
-    import {
-        Modal
-    } from 'flowbite';
-</script>
-<script>
-    var currentImageIndex = 0;
+    {{--
+    |--------------------------------------------------------------------------
+    | SCRIPTS
+    |--------------------------------------------------------------------------
+    --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+    <script src="/js/moment.js"></script>
+    <script>
+        import {
+            Modal, Popover
+        } from 'flowbite';
+    </script>
+    <script>
+        var currentImageIndex = 0;
     var images = document.querySelectorAll('.product-image');
 
     function showNextImage() {
@@ -175,5 +194,8 @@
 
         // Setelah tombol diklik, ubah kelasnya untuk membuatnya tetap merah
         $(this).find('.love-icon').addClass('text-red-500');
+        $(this).find('.love-icon').addClass('bg-red-50');
+        $(this).find('.love-icon').addClass('border');
+        $(this).find('.love-icon').addClass('border-red-500');
     });
-</script>
+    </script>
